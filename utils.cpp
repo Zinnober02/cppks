70f0303ab8f9d572caa414103973935ce47d5430
Zinnober <zinnober@foxmail.com>
2023-10-17 20:58:30 +0800

    颜色

--- utils.cpp
+++ utils.cpp
@@ -14,6 +14,11 @@ std::filesystem::path utils::getDirPath(int index1, int index2, int id) {
 		create_directories(dir_path);
 		std::cout << "成功创建目录" << std::endl;
 	}
-	std::cout << dir_path << std::endl;
+	//std::cout << dir_path << std::endl;
 	return dir_path;
+}
+
+void rgbSet(int wr, int wg, int wb, int br, int bg, int bb) {						//设置RGB
+	printf("\033[38;2;%d;%d;%dm\033[48;2;%d;%d;%dm", wr, wg, wb, br, bg, bb);
+	//\033[38   表示前景，\033[48   表示背景，三个%d表示混合的数
 }
